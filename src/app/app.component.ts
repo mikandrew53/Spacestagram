34ffa11e84cae2a6c3d8eafc051e3c35f2dd65cc
Andrew Mikaeel <andrew.mikaeel@ontariotechu.net>
2021-09-18 17:39:46 -0400

    Vector animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -8,6 +8,12 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent { 
+  @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      event.target.innerWidth < 1570 ? this.mobile = true : this.mobile = false;    
+  }
+
+  mobile: boolean = (window.innerWidth < 1570);
   ngOnInit(): void {
     window.scrollTo(0, 0);
   }
